d7be4ef58137d22116e31e1d0ec5347ce9cca81e
Marcos Antonio <assismarcos1975@gmail.com>
2024-01-12 11:29:30 -0300

    Readme.md update, code minor changes

--- VoteReport/Program.cs
+++ VoteReport/Program.cs
@@ -6,19 +6,19 @@ switch (args.Length)
 {
     case 1 when args[0] == "--help":
         Console.WriteLine("Usage: VoteReport [--files-location <String>] [--help]\n");
-        Console.WriteLine("VoteReport {0}\n", typeof(Program).Assembly.GetName().Version);
+        PrintVersion();    
         Console.WriteLine("Options:");
         Console.WriteLine("  --files-location <String>  Directory of CSV source files. Default value is the application directory");
         Console.WriteLine("  --help                     Show help message\n");
         return;
     case 2 when args[0] == "--files-location":
-        Console.WriteLine("VoteReport {0}\n", typeof(Program).Assembly.GetName().Version);
         filesLocation = args[1];
         break;
 }
 
 var voteReportService = new VoteReportService(new CsvRepository(), filesLocation);
 
+PrintVersion();
 Console.WriteLine("Validating CSV files");
 var validationResult = voteReportService.ValidateFiles();
 if (validationResult.Any())
@@ -32,3 +32,8 @@ if (validationResult.Any())
 Console.WriteLine("Generating CSV files at {0}", filesLocation);
 voteReportService.GenerateReports();
 Console.WriteLine("CSV files generated.\n");
+
+static void PrintVersion()
+{
+    Console.WriteLine("VoteReport {0}\n", typeof(Program).Assembly.GetName().Version);
+}
